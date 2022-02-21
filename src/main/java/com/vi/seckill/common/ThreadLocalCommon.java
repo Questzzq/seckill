551b3db3e81db77b862156c2556ccab0dac3515d
Questzzq <1073371323@qq.com>
2022-02-21 19:55:19 +0800

    缓存当前登录者ID及添加日志切面

---
+++ src/main/java/com/vi/seckill/common/ThreadLocalCommon.java
@@ -0,0 +1,9 @@
+package com.vi.seckill.common;
+/**
+*
+* @description  ThreadLocalCommon
+* @author Eric Tseng
+* @since  2022/2/21 18:46
+*/
+public class ThreadLocalCommon {
+}
---
+++ src/main/java/com/vi/seckill/handler/LogInterceptor.java
@@ -0,0 +1,9 @@
+package com.vi.seckill.handler;
+/**
+*
+* @description  LogInterceptor
+* @author Eric Tseng
+* @since  2022/2/21 19:13
+*/
+public class LogInterceptor {
+}
